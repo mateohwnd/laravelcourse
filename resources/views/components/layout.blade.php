6fe1c34ef391ccb0447ed1e2b5ee9b4087d9dec1
mateohwnd <christianhwnd@gmail.com>
2024-06-10 06:54:28 +0800

    added alpine js functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,16 +6,38 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>{{ env('APP_NAME') }}</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="bg-slate-100 text-slate-900">
 <header>
   <nav>
     <a href="{{ route('home') }}" class="nav-link">Home</a>
-    <div class="flex items-center gap-4">
-      <a href="{{ route('login') }}" class="nav-link">Login</a>
-      <a href="{{ route('register') }}" class="nav-link">Register</a>
-    </div>
+
+    @auth
+      <div class="relative grid place-items-center" x-data="{ open: false }">
+        {{-- Dropdown menu button --}}
+        <button x-on:click="open = !open" type="button" class="round-btn">
+          <img src="https://picsum.photos/200" alt="">
+        </button>
+
+        {{-- Dropdown menu --}}
+        <div x-show="open" @click.outside="open = false"
+             class="bg-white shadow-lg absolute top-10 right-0 rounded-lg overflow-hidden font-light">
+          <p class="username">{{ auth()->user()->username }}</p>
+
+          <a href="" class="block hover:bg-slate-100 pl-4 pr-8 py-2 mb-1">Dashboard</a>
+
+        </div>
+      </div>
+    @endauth
+
+    @guest
+      <div class="flex items-center gap-4">
+        <a href="{{ route('login') }}" class="nav-link">Login</a>
+        <a href="{{ route('register') }}" class="nav-link">Register</a>
+      </div>
+    @endguest
   </nav>
 </header>
 
